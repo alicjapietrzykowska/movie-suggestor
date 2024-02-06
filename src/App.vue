2f928821e53f23073c6ac729d9ea30d65c644cfc
Alicja <alicja.x.lukaszewska@gmail.com>
2024-02-07 00:23:06 +0100

    Usage and basic styling of the app ready

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container my-4">
     <Header />
     <div class="notification">
       <Form />
--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,27 +1,45 @@
 <template>
-  <form @submit="findMovie" class="is-flex is-flex-direction-column">
-    <label class="label mb-4">Genre</label>
-    <input class="input mb-4" type="text" v-model="genre" />
-    <label class="label mb-4">Details</label>
-    <textarea class="textarea mb-4" type="text" v-model="details" />
-    <div class="mb-4">
-      <input id="spoilers" type="checkbox" class="checkbox mr-3" v-model="showSpoilers" />
-      <label for="spoilers">Tell spoilers?</label>
-    </div>
-    <button class="button is-primary">Submit</button>
-  </form>
+  <div>
+    <form @submit="findMovie" class="is-flex is-flex-direction-column">
+      <label class="label mb-4">Genre</label>
+      <input class="input mb-4" type="text" v-model="genre" />
+      <label class="label mb-4">Details</label>
+      <textarea class="textarea mb-4" type="text" v-model="details" />
+      <div class="mb-4">
+        <input id="spoilers" type="checkbox" class="checkbox mr-3" v-model="showSpoilers" />
+        <label for="spoilers">Show spoilers?</label>
+      </div>
+      <button class="button is-primary" :class="{ 'is-loading': isLoading }">Submit</button>
+    </form>
+
+    <p class="mt-6 content" v-html="movieSuggestion" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { makeRequest } from '../../services/openaiService'
 
 const genre = ref<string>('')
 const details = ref<string>('')
+const movieSuggestion = ref<string>('')
 const showSpoilers = ref<boolean>(false)
 
-const findMovie = (event: Event) => {
+const isLoading = ref<boolean>(false)
+
+const getPrompt = (): string => {
+  const prompt = `Suggest a move from ${genre.value} genre. The movie should be about ${details.value}.`
+  return showSpoilers.value ? prompt : `${prompt} You are not going to tell any spoilers.`
+}
+
+const findMovie = async (event: Event): Promise<void> => {
+  isLoading.value = true
   event.preventDefault()
-  console.log({ genre: genre.value, details: details.value, showSpoilers: showSpoilers.value })
+
+  const response = await makeRequest(getPrompt())
+  isLoading.value = false
+  console.log(response)
+  movieSuggestion.value = response.trim()
 }
 </script>
 
